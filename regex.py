bd454ae3bc311c413129d2dde8193074962b651a
Bram Musuko Panjaitan <32943475+bmusuko@users.noreply.github.com>
2019-04-17 17:06:24 +0700

    Add files via upload

---
+++ regex.py
@@ -0,0 +1,60 @@
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import re
+
+def cekRegex(query,ask):
+	kata = len(query)
+	c = 0
+	for q in query:
+		#print(q)
+		if (re.search(r'%s' % (q), ask,re.IGNORECASE)):
+			c += 1
+	return (c == kata)
+
+def main():
+	factory = StemmerFactory()
+	stemmer = factory.create_stemmer()
+	question = open("ask_indo.txt","r")
+	answer = open("ans_indo.txt","r")
+	stopword = open("stopword.txt","r")
+	sw = []
+	pertanyaan = []
+	jawaban = []
+	for s in stopword:
+		sw.append(s.rstrip())
+	for s in question:
+		pertanyaan.append(s.rstrip())
+	for s in answer:
+		jawaban.append(s.rstrip())
+
+	query = input()
+	query = stemmer.stem(query)
+	
+	query = query.split()
+	qtemp = query.copy()
+	
+	# print (query)
+	for q in (query):
+		# print(q)
+		for s in (sw):
+			if(q == s):
+				qtemp.remove(q)
+				break
+
+	#print (qtemp)
+	c = 0
+	found = False
+	#print(pertanyaan)
+	for s in pertanyaan:
+		#print(s)
+		if (cekRegex(query,s)):
+			found = True
+			#print("hore")
+			break
+		else :
+			#print("ga ketemu")
+			c += 1
+	#print(c)
+	if (found):
+		print(jawaban[c])	
+
+main()
